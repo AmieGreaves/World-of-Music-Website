d2385ef0fbaebeab41e750d640dc5b774e49f82e
AmieGreaves <30555366+AmieGreaves@users.noreply.github.com>
2018-01-04 09:06:19 +0000

    Add files via upload

---
+++ Amie Unit 15 B/Amie Unit 15 B/JS/main.js
@@ -0,0 +1,14 @@
+function imgLarge(i) {
+    i.style.height = "100%";
+    i.style.width = "100%";
+}
+
+function imgRegular(i) {
+    i.style.height = "75%";
+    i.style.width = "75%";
+}
+
+function popup() {
+    //var popup = document.getElementById("myPopup");
+    alert("Thankyou for getting in touch! We will get back to you as soon as we can");
+}
